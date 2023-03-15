e101ec797089e341a968f3d650c7ee538d8da367
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-03-15 19:57:43 +0000

    change `X11Frame` to use whole messages as frames

--- src/client/rw.rs
+++ src/client/rw.rs
@@ -6,37 +6,69 @@ use crate::Client;
 use bytes::Bytes;
 
 pub(crate) enum X11Frame {
-	RequestHeader {
+	/// Note: requests are never read by X.RS; requests are received by X
+	/// servers, while X.RS is a client library. The only way to tell apart
+	/// requests vs. replies & events is whether they are received by an X
+	/// client or whether they are received by the X server.
+	Request {
+		/// The major opcode identifying the type of request (for core requests)
+		/// or the extension that defines the request (for extension requests).
 		major_opcode: u8,
+		/// A single byte in the header which may be used for additional data.
 		metabyte: u8,
+		/// The length of the request in blocks (units of 4 bytes).
+		///
+		/// If the big-requests extension is enabled and this is `0`, then the
+		/// next block is the length of the request instead, but as a `u32`
+		/// value.
 		length: u16,
-	},
-	ReplyHeader {
-		metabyte: u8,
-		sequence: u16,
-		length: u32,
-	},
-	EventHeader {
-		code: u8,
-		metabyte: u8,
-		sequence: u16,
-	},
-	ErrorHeader {
-		code: u8,
-		sequence: u16,
-		metablock: [u8; 4],
-		minor_opcode: u16,
-		major_opcode: u8,
+
+		/// Additional data contained in the request, if any.
+		chunk: Bytes,
 	},
 
-	ReplyEventChunk {
-		chunk: [u8; 24],
-	},
-	ErrorChunk {
-		chunk: [u8; 21],
-	},
-	Chunk {
+	Reply {
+		/// A single byte in the header which may be used for additional data.
+		metabyte: u8,
+		/// The sequence number which identifies when the request that generated
+		/// the reply was sent.
+		sequence: u16,
+		/// The length of any additional data after the first 8 blocks (32
+		/// bytes) in the reply in blocks (units of 4 bytes).
+		length: u32,
+
+		/// Data contained in the reply.
 		chunk: Bytes,
+	},
+
+	Event {
+		/// The code uniquely identifying the type of event.
+		code: u8,
+		/// Data contained in the event.
+		chunk: [u8; 31],
+	},
+
+	Error {
+		/// The code uniquely identifying the type of error.
+		code: u8,
+		/// The sequence number which identifies when the last request sent that
+		/// relates to the generation of this error was sent.
+		sequence: u16,
+		/// A single block (4 bytes) in the header which may be used for
+		/// additional data.
+		metablock: [u8; 4],
+
+		/// The minor opcode identifying the type of the last request sent that
+		/// relates to the generation of this error within its extension (`0`
+		/// for core requests).
+		minor_opcode: u16,
+		/// The major opcode identifying the type of (for core requests) or the
+		/// extension which defines (for extension requests) the last request
+		/// sent that relates to the generation of this error.
+		major_opcode: u8,
+
+		/// Additional data contained in the error.
+		chunk: [u8; 21],
 	},
 }
 
@@ -50,7 +82,7 @@ impl Client {
 				return Ok(Some(frame));
 			}
 
-			if self.stream.read_buf(&mut self.buffer).await? {
+			if self.stream.read_buf(&mut self.buffer).await? == END_OF_STREAM {
 				if self.buffer.is_empty() {
 					return Ok(None);
 				} else {
